69a350b5e950fab0cf00e69b8500e1f1a9405347
carrymaniac <838537276@qq.com>
2019-06-21 17:25:01 +0800

    完成35-37

--- src/Chap4/q30.java
+++ src/Chap4/q30.java
@@ -7,6 +7,7 @@ import java.util.Stack;
  * @DATE 2019/6/15 15:32
  **/
 //定义栈的数据结构，请在该类型中实现一个能够得到栈最小元素的 min 函数。
+    //定义一个辅助栈,每次插入新元素的时候 判断当前的最小元素,即取辅助栈栈顶元素和新插入的元素进行对比判断.
 public class q30 {
     private Stack<Integer> dataStack = new Stack<>();
     private Stack<Integer> minStack = new Stack<>();
--- src/Chap4/q32_1.java
+++ src/Chap4/q32_1.java
@@ -11,6 +11,7 @@ import java.util.Queue;
  * @DATE 2019/6/15 16:04
  **/
 //从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+    //用一个队列来承接遍历的节点,当队列不为空时,取出队列的节点,将其输出到数组中,并将其左右节点放回队列中
 public class q32_1 {
     public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
